21ff9f0a697f4acf053d8e99d6977e65b2123db8
gernsdorfer <larswiedemann@gmx.net>
2015-03-26 20:42:50 +0100

    refactor style

--- app/scripts/directive/group/admin.js
+++ app/scripts/directive/group/admin.js
@@ -3,24 +3,25 @@
 /**
  * @author Lars Wiedemann
  * @ngdoc directive
- * @name unchatbar-contact.directive:unContactGroupSelected
+ * @name unchatbar-contact.directive:unContactGroupAdmin
  * @restrict E
  * @description
  *
  * selected group
  *
  */
-angular.module('unchatbar-contact').directive('unContactGroupSelected', [
+angular.module('unchatbar-contact').directive('unContactGroupAdmin', [
     function () {
         return {
             restrict: 'E',
             replace: false,
-            templateUrl: 'views/unchatbar-contact/group/selected.html',
+            templateUrl: 'views/unchatbar-contact/group/admin.html',
             controller: 'unContactGroup',
             link: function (scope) {
 
                 function updateScope() {
                     scope.getGroup();
+                    scope.getClientMap();
                 }
 
                 scope.$on('$stateChangeSuccess', function () {
