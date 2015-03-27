a81472d75726e158365d04f93e55101f9c8d69e0
gernsdorfer <larswiedemann@gmx.net>
2015-03-27 19:39:47 +0100

    add translation

---
+++ app/scripts/translations.js
@@ -0,0 +1,5 @@
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('de', {"Add group":"Gruppe hinzufügen","Enter groupname name":"Gib hier den Gruppenname an","Profile Picture":"Profilebild","Update":"Ändern","a short description":"Kurzbeschreibung","admin group":"Gruppen Administration","group name":"Gruppenname","only group admin can update a group":"Nur Gruppen-Administratoren können Änderungen an der Gruppe vornehmen","remove":"löschen","short description":"Kurzbeschreibung","users in group":"Nutzer in Gruppe"});
+/* jshint +W100 */
+}]);
