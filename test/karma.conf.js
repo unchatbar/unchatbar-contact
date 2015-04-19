ba69bccf3db3058af753c68d8fea25cf43c519cc
gernsdorfer <larswiedemann@gmx.net>
2015-04-19 13:38:04 +0200

    Release v0.1.1

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,11 +31,11 @@ module.exports = function (config) {
             'bower_components/angular-gettext/dist/angular-gettext.js',
             'bower_components/peerjs/peer.js',
             'bower_components/unchatbar-connection/app/scripts/app.js',
-            'bower_components/unchatbar-connection/app/scripts/run.js',
+
             'bower_components/unchatbar-connection/app/scripts/template.js',
             'bower_components/unchatbar-connection/app/scripts/translations.js',
             'bower_components/unchatbar-connection/app/scripts/provider/broker.js',
-            'bower_components/unchatbar-connection/app/scripts/provider/data-connection.js',
+            'bower_components/unchatbar-connection/app/scripts/service/data-connection.js',
             'bower_components/unchatbar-connection/app/scripts/service/peer.js',
             'bower_components/unchatbar-connection/app/scripts/directive/dialer.js',
             'bower_components/unchatbar-connection/app/scripts/directive/login.js',
